cd60808a2d5aaebb927305a280be0fa7c735c94c
EhsanSZ <e.seyedzadeh73@gmail.com>
2024-05-12 01:11:10 +0330

    Identity Provider

---
+++ IdentityProvider/src/IdentityServer/Program.cs
@@ -0,0 +1,49 @@
+﻿using IdentityServer;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
+
+Log.Information("Starting up");
+
+try
+{
+    var builder = WebApplication.CreateBuilder(args);
+    var serviceName = "news.cms.identiry.provider";
+    var serviceVersion = "1.0.0";
+    // Configure important OpenTelemetry settings, the console exporter, and instrumentation library
+    builder.Services.AddOpenTelemetryTracing(tracerProviderBuilder =>
+    {
+        tracerProviderBuilder
+        .AddConsoleExporter().AddJaegerExporter()
+        .AddSource(serviceName)
+        .SetResourceBuilder(
+            ResourceBuilder.CreateDefault()
+                .AddService(serviceName: serviceName, serviceVersion: serviceVersion))
+        .AddHttpClientInstrumentation()
+        .AddAspNetCoreInstrumentation()
+        .AddSqlClientInstrumentation();
+    });
+    builder.Host.UseSerilog((ctx, lc) => lc
+        .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}")
+        .Enrich.FromLogContext()
+        .ReadFrom.Configuration(ctx.Configuration));
+
+    var app = builder
+        .ConfigureServices()
+        .ConfigurePipeline();
+    
+    app.Run();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Unhandled exception");
+}
+finally
+{
+    Log.Information("Shut down complete");
+    Log.CloseAndFlush();
+}
